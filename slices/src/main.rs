2aa2ff0a15ebb3808815ad0f1c7497f1eb21484e
Jabbslad <Jabbslad jabbslad@gmail.com>
2020-11-18 23:56:51 +0000

    added slices

---
+++ slices/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+
+    let words = String::from("Hello World");
+
+    let first_idx = first_word(&words);
+    println!("{}", first_idx);
+
+    let first_idx = first_word2(&words);
+    println!("{}", first_idx);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn first_word2(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    return &s[..]
+}
